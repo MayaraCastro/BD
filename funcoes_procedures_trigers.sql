32e797db12767d7b981b6279b4eec6e307b14e46
Giuseppe Fiorentino Neto <netogiuseppe@hotmail.com>
2018-07-29 20:44:25 -0300

    Update funcoes_procedures_trigers.sql

--- funcoes_procedures_trigers.sql
+++ funcoes_procedures_trigers.sql
@@ -22,6 +22,29 @@ create function cliente_apto (cod int, pais varchar(50), dt_inicio date, dt_fim 
 	end if;
  end;
 $$
+#2.   para retornar se o CLIENTE esta em dias com seus pagamentos de FATURAS
+
+create function cliente_apto (cod int) returns boll
+	begin
+		--0 pago
+		--!=0 nao pago
+		
+		select f.status
+		from fatura f join pacote p join  clienteJuridico_compra cj join clienteFisico_compra cf
+		on cj.codcliente==cod or cf.codcliente==cod
+		where
+			f.codpacote = p.codigo
+		
+		if f.status==0 then
+			return true;
+		else
+			return false;
+		end if;		
+
+	end;
+$$
+
+
 
 -- PROCEDURES
 #1. para gerar malas diretas com textos promocionais em função do consumo de pacotes dos CLIENTEs
@@ -37,6 +60,20 @@ create procedure gera_malaDireta(in tipo_pacote int)
 			INSERT INTO malaDireta (texto, dt_criacao) VALUES ('Promocao Individual', curdate());
 		end if;
         end if;
+    end;
+$$ 
+
+#2. para verificar se o "status" de uma FATURA em função dos PAGAMENTOS 
+
+delimiter $$
+create procedure verificarStatus(in status int)
+	begin
+		if( status = 0 ) then
+			INSERT INTO fatura (status) VALUES (0);
+        
+	        else  then
+			INSERT INTO fatura (status) VALUES (1);
+	        end if;
     end;
 $$ 
 
@@ -127,6 +164,55 @@ for each row
 begin
 	if(old.cod_agencia is not null)then
 		update agencia set num_funcionarios = num_funcionarios - 1 where CNPJ = old.cod_agencia;
+    end if;
+end;
+$$
+
+#2.  Para atualizar o  atributo "numero_clientes" de AGENCIA 
+delimiter $$
+create trigger atualiza_num_clientes_fisicos_delete 
+before delete on cliente_fisico
+for each row
+begin
+
+	if(old.cod_cliente is not null)then
+		update agencia set num_clientes_fisicos = num_clientes_fisicos - 1 where CNPJ = old.cod_agencia;
+    end if;
+end;
+$$
+
+delimiter $$
+create trigger atualiza_num_clientes_fisicos_insert 
+after insert on cliente_fisico
+for each row
+begin
+	if(new.cod_agencia is not null)then
+		update agencia set num_clientes_fisicos = num_clientes_fisicos + 1 where CNPJ = new.cod_agencia;
+    end if;
+end;
+$$
+
+
+
+delimiter $$
+create trigger atualiza_num_clientes_juridicos_delete 
+before delete on cliente_juridico
+for each row
+begin
+
+	if(old.cod_agencia is not null)then
+		update agencia set num_cliente_juridico = num_cliente_juridico - 1 where CNPJ = old.cod_agencia;
+    end if;
+end;
+$$
+
+delimiter $$
+create trigger atualiza_num_clientes_juridicos_insert 
+after insert on cliente_juridico
+for each row
+begin
+	if(new.cod_agencia is not null)then
+		update agencia set num_cliente_juridico = num_cliente_juridico + 1 where CNPJ = new.cod_agencia;
     end if;
 end;
 $$
