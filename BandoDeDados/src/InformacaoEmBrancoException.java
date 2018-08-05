3ae9c4950f722ee5a55e30932b4c4fa8ea56c044
Unknown <netogiuseppe@hotmail.com>
2018-08-04 22:07:40 -0300

    Minhas Adds do codigo em Java

---
+++ BandoDeDados/src/InformacaoEmBrancoException.java
@@ -0,0 +1,8 @@
+
+public class InformacaoEmBrancoException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+		public InformacaoEmBrancoException() {
+			super("É preciso preencher todos as informações");
+		}
+}
