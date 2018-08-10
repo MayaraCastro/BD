28f69c7913ebe1acad0bc6dedf4f84f6799525b2
Mayara Castro <maaycastro@hotmail.com>
2018-08-10 18:48:01 -0300

    organização

--- BandoDeDados/src/InformacaoEmBrancoException.java
+++
@@ -1,8 +0,0 @@
-
-public class InformacaoEmBrancoException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-		public InformacaoEmBrancoException() {
-			super("É preciso preencher todos as informações");
-		}
-}
